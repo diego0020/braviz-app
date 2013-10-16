e542abdf7e543f9117c7d42d6144285f571f109e
Diego <Diego@dieg8>
2013-10-16 17:07:00 -0400

    finished amygdala_explore

---
+++ braviz/visualization/create_lut.py
@@ -0,0 +1,39 @@
+from __future__ import division
+import vtk
+import colorbrewer
+
+
+__author__ = 'Diego'
+
+
+def get_colorbrewer_lut(minimum,maximum,scheme,steps,continuous=True,nan_color=(1.0,0,0)):
+    #build lut
+    scalar_lookup_table = vtk.vtkColorTransferFunction()
+    scalar_lookup_table.ClampingOn()
+    scalar_lookup_table.SetColorSpaceToLab()
+    scalar_lookup_table.SetRange(minimum, maximum)
+    scalar_lookup_table.Build()
+    assert steps>1
+    sharpness=1
+    if continuous is True:
+        sharpness=0
+    #load colorbrewer scheme
+    try:
+        cb_list=getattr(colorbrewer,scheme)
+    except AttributeError:
+        print "Unknown scheme %s, please look at http://colorbrewer2.org/ for available strings"%scheme
+        raise
+
+    try:
+        cb_list=cb_list[steps]
+    except KeyError:
+        print "this scheme is not available for %d steps"%steps
+        raise
+
+    delta=(maximum-minimum)/(steps-1)
+    for i in range(steps):
+        c = cb_list[i]
+        #                                   x            ,r   ,g   , b, midpoint, sharpness
+        scalar_lookup_table.AddRGBPoint(minimum+delta*i,c[0],c[1],c[2], 0.5,  sharpness)
+    scalar_lookup_table.SetNanColor(1.0, 0, 0)
+    return scalar_lookup_table
--- concept_tests/amygdalas_explore.py
+++ concept_tests/amygdalas_explore.py
@@ -1,10 +1,15 @@
 from __future__ import division
 import braviz
+from braviz.visualization.create_lut import get_colorbrewer_lut
 import vtk
 import random
 import math
-
+from os.path import join as path_join
+import numpy as np
 __author__ = 'Diego'
+
+scalar_column='WMIIQ'
+
 reader=braviz.readAndFilter.kmc40AutoReader()
 viewer=braviz.visualization.simpleVtkViewer()
 
@@ -12,7 +17,22 @@ subj_ids=reader.get('ids')
 
 amygdalas={}
 for subj in subj_ids:
-    amygdalas[subj]=reader.get('model',subj,name='Left-Amygdala',space='talairach')
+    #amygdalas[subj]=reader.get('model',subj,name='Left-Amygdala',space='talairach')
+    amyg1=reader.get('model',subj,name='Left-Amygdala',space='world')
+    #center at zero
+    center=amyg1.GetCenter()
+    trans = vtk.vtkTransformPolyDataFilter()
+    t = vtk.vtkTransform()
+    t.Identity()
+    t.Translate(center)
+    t.Inverse()
+    trans.SetTransform(t)
+    trans.SetInputData(amyg1)
+    trans.Update()
+    amyg2=trans.GetOutput()
+    amygdalas[subj]=amyg2
+
+
 
 actors_dict={}
 subj_dir={}
@@ -22,8 +42,25 @@ for subj in subj_ids:
     subj_dir[subj]=actor
 
 #color
+csv_file=path_join(reader.getDataRoot(),'test_small.csv')
+csv_codes=braviz.readAndFilter.read_csv.get_column(csv_file,'code')
+csv_data=braviz.readAndFilter.read_csv.get_column(csv_file,scalar_column,True)
+#build dict
+csv_data_dict={}
+for code,datum in zip(csv_codes,csv_data):
+    if not math.isnan(datum):
+        csv_data_dict[code]=datum
+
+#build lut
+scalar_lut=get_colorbrewer_lut(min(csv_data),max(csv_data),'PuBuGn',9)
+
 def get_color(id):
-    return [random.random() for i in range(3)]
+    scalar=csv_data_dict.get(id,float('nan'))
+    output=scalar_lut.GetColor(scalar)
+    #output=map(lambda x:x/255,output)
+    return output
+    #return [random.random() for i in range(3)]
+
 
 for actor,id in actors_dict.iteritems():
     actor.GetProperty().SetColor(get_color(id))
@@ -38,7 +75,8 @@ balloon_widget.SetInteractor(viewer.iren)
 balloon_widget.On()
 
 for actor,id in actors_dict.iteritems():
-    balloon_widget.AddBalloon(actor,id)
+    message="%s\n%s : %.2f"%(id,scalar_column,csv_data_dict.get(id,float('nan')))
+    balloon_widget.AddBalloon(actor,message)
 #order
 
 #calculate bounds
@@ -46,7 +84,7 @@ def get_max_diagonal(actor):
     Xmin, Xmax, Ymin, Ymax, Zmin, Zmax=actor.GetBounds()
     return math.sqrt((Xmax-Xmin)**2+(Ymax-Ymin)**2+(Zmax-Zmin)**2)
 
-max_space=max([get_max_diagonal(actor) for actor in actors_dict.keys()])
+max_space=max([get_max_diagonal(actor) for actor in actors_dict])
 max_space*=0.8
 #calculate renWin proportions
 width,height=viewer.renWin.GetSize()
@@ -57,7 +95,9 @@ area_per_object=total_area/total_objects
 n_row=round(math.sqrt(len(subj_ids)/row_proportion))
 n_col=math.ceil(len(subj_ids)/n_row)
 
-sorted_subj=sorted(subj_ids,key=lambda x:int(x))
+sorted_subj=sorted(subj_ids,key=lambda x:csv_data_dict.get(x,float('+inf')))
+print sorted_subj
+positions_dict={}
 for i,subj in enumerate(sorted_subj):
     actor=subj_dir[subj]
     column=i%n_col
@@ -65,6 +105,9 @@ for i,subj in enumerate(sorted_subj):
     x=column*max_space
     y=row*max_space
     actor.SetPosition(x ,y , 0)
+    positions_dict[actor]=(x,y,0)
+
+
 
 
 
@@ -76,5 +119,84 @@ cam1.SetPosition(n_col*max_space/2,n_row*max_space/2,-200)
 cam1.SetViewUp(0,-1,0)
 
 #interaction
-viewer.iren.SetInteractorStyle(vtk.vtkInteractorStyleImage())
+#viewer.iren.SetInteractorStyle(vtk.vtkInteractorStyleImage())
+viewer.iren.SetInteractorStyle(vtk.vtkInteractorStyleTrackballActor())
+
+first_time=True
+modified_actor=None
+
+def register_change(caller=None,envent=None):
+    global modified_actor,panning
+    panning = False
+    if modified_actor is not None:
+        return
+    modified_actor=caller
+
+
+def after_intareaction(caller=None,event=None):
+    global modified_actor
+    if modified_actor is not None:
+        mimic_actor(modified_actor)
+
+    modified_actor=None
+
+def mimic_actor(caller=None,event=None):
+    global first_time
+    orig_pos=np.array(positions_dict[caller])
+    for ac in actors_dict:
+        if ac is not caller:
+            delta_pos=caller.GetPosition()-orig_pos
+            #ac.SetPosition(positions_dict[ac]+delta_pos)
+            ac.SetOrientation(caller.GetOrientation())
+            ac.SetScale(caller.GetScale())
+
+for actor in actors_dict:
+    actor.AddObserver(vtk.vtkCommand.PickEvent,register_change)
+
+def wheel_zoom(caller=None,event=None):
+    factor=0.5
+    if event=='MouseWheelForwardEvent':
+        cam1.SetParallelScale(cam1.GetParallelScale()*factor)
+    else:
+        cam1.SetParallelScale(cam1.GetParallelScale() / factor)
+    viewer.iren.Render()
+
+panning=False
+panning_start_pos=None
+cam_pos_start_pos=None
+cam_focal_start_pos=None
+def pan(caller=None, event=None):
+    global panning,panning_start_pos,cam_focal_start_pos,cam_pos_start_pos
+    if event=='MiddleButtonPressEvent':
+        event_pos_x,event_pos_y=caller.GetEventPosition()
+        if(viewer.ren.PickProp(event_pos_x,event_pos_y) is not None):
+            panning=False
+            return
+        panning=True
+        panning_start_pos=np.array(caller.GetEventPosition())
+        cam_pos_start_pos=np.array(cam1.GetPosition())
+        cam_focal_start_pos=np.array(cam1.GetFocalPoint())
+    else:
+        if panning==False:
+            return
+        if event=='MiddleButtonReleaseEvent':
+            panning=False
+    delta=caller.GetEventPosition()-panning_start_pos
+    delta=(delta[0],-delta[1],0)
+    cam1.SetPosition(cam_pos_start_pos-delta)
+    cam1.SetFocalPoint(cam_focal_start_pos-delta)
+    caller.Render()
+    #print "pan: %s"%event
+viewer.iren.AddObserver(vtk.vtkCommand.EndInteractionEvent, after_intareaction)
+viewer.iren.AddObserver(vtk.vtkCommand.MouseWheelForwardEvent, wheel_zoom)
+viewer.iren.AddObserver(vtk.vtkCommand.MouseWheelBackwardEvent, wheel_zoom)
+viewer.iren.AddObserver(vtk.vtkCommand.MiddleButtonPressEvent, pan)
+viewer.iren.AddObserver(vtk.vtkCommand.MouseMoveEvent, pan)
+viewer.iren.AddObserver(vtk.vtkCommand.MiddleButtonReleaseEvent, pan,100)
+
+def print_event(caller=None,event=None):
+    print event
+
+
+
 viewer.start()
