b38b95f2da11b87dd9a6ffa31d6a1cb875eda70f
Diego <Diego@dieg8>
2013-10-24 21:07:45 -0400

    orden del tutorial frame

--- concept_tests/tutorial_yoyis_frame.py
+++ concept_tests/tutorial_yoyis_frame.py
@@ -10,15 +10,44 @@ from vtk.tk.vtkTkRenderWindowInteractor import \
 reader=braviz.readAndFilter.kmc40AutoReader()
 
 
-#leer putamen
-putamen=reader.get('model','093',name='Left-Putamen')
-
 #crear visualizador
 renWin=vtk.vtkRenderWindow()
 ren=vtk.vtkRenderer()
 renWin.AddRenderer(ren)
 ren.SetBackground(0.5,0,0.5)
 
+
+##=========GUI============================
+root = tk.Tk()
+root.title('Tutorial Yoyis')
+
+yoyis_frame=tk.Frame(root)
+render_widget = vtkTkRenderWindowInteractor(yoyis_frame,
+                                            rw=renWin, width=600,
+                                            height=600)
+
+render_widget.grid(sticky='nsew')
+yoyis_frame.rowconfigure(0,weight=1)  # para que crezca cuando crece la ventana
+yoyis_frame.columnconfigure(0,weight=1)
+
+yoyis_frame.grid(sticky='ewsn')
+root.rowconfigure(0,weight=1) # para que crezca cuando crece la ventana
+root.columnconfigure(0,weight=1)
+
+#para que no se queje al cerrar la ventana
+def clean_exit():
+    global renWin
+    print "adios"
+    renWin.Finalize()
+    del renWin
+    render_widget.destroy()
+    root.quit()
+    root.destroy()
+root.protocol("WM_DELETE_WINDOW", clean_exit)
+#=============================================
+
+#leer putamen
+putamen=reader.get('model','093',name='Left-Putamen')
 
 #agregar putamen al visualizador
 putamen_mapper=vtk.vtkPolyDataMapper()
@@ -27,7 +56,6 @@ putamen_actor=vtk.vtkActor()
 putamen_actor.SetMapper(putamen_mapper)
 ren.AddActor(putamen_actor)
 
-#iniciar visualizador
 
 #Agregar amygdala
 amygdala=reader.get('model','093',name='Right-Amygdala')
@@ -43,27 +71,6 @@ mri=reader.get('mri','093',format='vtk')
 image_plane=braviz.visualization.persistentImagePlane()
 image_plane.SetInputData(mri)
 
-##=========GUI================
-root = tk.Tk()
-root.title('Tutorial Yoyis')
-
-yoyis_frame=tk.Frame(root)
-render_widget = vtkTkRenderWindowInteractor(yoyis_frame,
-                                            rw=renWin, width=600,
-                                            height=600)
-
-render_widget.grid(sticky='nsew')
-yoyis_frame.grid(sticky='ewsn')
-
-def clean_exit():
-    global renWin
-    print "adios"
-    renWin.Finalize()
-    del renWin
-    render_widget.destroy()
-    root.quit()
-    root.destroy()
-root.protocol("WM_DELETE_WINDOW", clean_exit)
 
 #inicializar
 
