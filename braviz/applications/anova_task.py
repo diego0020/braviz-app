10f67e1fb2733e9e14ec5357975895645df0577e
Diego A <devnull@localhost>
2014-02-18 11:42:22 -0600

    work in outcome select dialog

--- braviz/applications/anova_task.py
+++ braviz/applications/anova_task.py
@@ -1,3 +1,4 @@
+from __future__ import division
 __author__ = 'Diego'
 
 import PyQt4.QtGui as QtGui
@@ -9,9 +10,10 @@ from PyQt4.QtGui import QMainWindow
 from braviz.interaction.qt_guis.anova import Ui_Anova_gui
 from braviz.interaction.qt_guis.outcome_select import Ui_SelectOutcomeDialog
 from braviz.interaction.qt_guis.nominal_details_frame import Ui_nominal_details_frame
+from braviz.interaction.qt_guis.rational_details_frame import Ui_rational_details
 
 import braviz.interaction.qt_models as braviz_models
-from braviz.readAndFilter.tabular_data import get_connection
+from braviz.readAndFilter.tabular_data import get_connection,get_data_frame
 
 class outcome_select_dialog(QtGui.QDialog):
     def __init__(self):
@@ -21,6 +23,10 @@ class outcome_select_dialog(QtGui.QDialog):
         self.vars_list_model=braviz_models.var_list_model()
         self.ui.tableView.setModel(self.vars_list_model)
         self.ui.tableView.activated.connect(self.update_right_side)
+        self.ui.var_type_combo.currentIndexChanged.connect(self.update_details)
+        self.var_name=None
+        self.details_ui=None
+        self.rational={}
     def update_right_side(self):
         curr_idx=self.ui.tableView.currentIndex()
         var_name=self.vars_list_model.data(curr_idx,QtCore.Qt.DisplayRole)
@@ -32,22 +38,85 @@ class outcome_select_dialog(QtGui.QDialog):
         cur=conn.cursor()
         cur.execute("SELECT is_real from variables where var_name=?",(var_name,))
         is_real=cur.fetchone()[0]
+        self.var_name=var_name
         if is_real is not None:
             print is_real
         else:
             print "unknown type, assuming real"
-            is_real=False
-        if not is_real:
-            self.create_nominal_details(var_name)
+            is_real=True
         conn.close()
+        if is_real:
+            self.ui.var_type_combo.setCurrentIndex(0)
+            self.update_details(0)
+        else:
+            self.ui.var_type_combo.setCurrentIndex(1)
+            self.update_details(1)
+
+    def update_details(self,index):
+        #is_real=self.ui.var_type_combo.currentIndex()
+        print index
+        print "===="
+        self.clear_details_frame()
+        if index==0:
+            QtCore.QTimer.singleShot(0 , self.create_real_details)
+        else:
+            QtCore.QTimer.singleShot(0 , self.create_nominal_details)
+    def clear_details_frame(self,layout=None):
+        if layout is None:
+            layout=self.ui.details_frame.layout()
+        if layout is None:
+            return
+        for i in reversed(xrange(layout.count())):
+            item = layout.itemAt(i)
+            if isinstance(item, QtGui.QWidgetItem):
+                item.widget().close()
+            elif isinstance(item, QtGui.QSpacerItem):
+                pass
+            else:
+                self.clearLayout(item.layout())
+            layout.removeItem(item)
+        layout.deleteLater()
+    def guess_max_min(self):
+        data=get_data_frame(self.var_name)
+        mini=data.min()[0]
+        maxi=data.max(skipna=True)[0]
+        medi=data.median()[0]
+        self.rational["max"]=maxi
+        self.rational["min"]=mini
+        self.rational["opt"]=medi
+        self.details_ui.maximum_val.setValue(maxi)
+        self.details_ui.minimum_val.setValue(mini)
+        self.details_ui.optimum_val.setValue(medi)
+
+        self.update_slider_position(medi)
+
+    def update_slider_position(self,value):
+        mini=self.rational.get("min",0)
+        maxi=self.rational.get("max",100)
+        self.details_ui.horizontalSlider.setValue(int((value-mini)/(maxi-mini)*100))
+
+    def update_optimum(self,value):
+        mini=self.rational.get("min",0)
+        maxi=self.rational.get("max",100)
+        real_value=mini+(maxi-mini)*value/100
+        self.details_ui.optimum_val.setValue(real_value)
+
     def create_real_details(self):
-        pass
-    def create_nominal_details(self,var_name):
+        print "creating real details"
+        details_ui=Ui_rational_details()
+        details_ui.setupUi(self.ui.details_frame)
+        self.details_ui=details_ui
+        self.details_ui.optimum_val.valueChanged.connect(self.update_slider_position)
+        self.details_ui.horizontalSlider.sliderMoved.connect(self.update_optimum)
+        self.guess_max_min()
+    def create_nominal_details(self):
+        var_name=self.var_name
         print "creating details"
         model=braviz_models.nominal_variables_meta(var_name)
         details_ui=Ui_nominal_details_frame()
         details_ui.setupUi(self.ui.details_frame)
         details_ui.labels_names_table.setModel(model)
+        self.details_ui=details_ui
 
 
 
--- braviz/readAndFilter/tabular_data.py
+++ braviz/readAndFilter/tabular_data.py
@@ -4,6 +4,7 @@ import pandas
 import sqlite3
 from pandas.io import sql
 import os
+import pandas as pd
 
 def get_variables(reader=None):
 
@@ -19,3 +20,19 @@ def get_connection(reader=None):
     path=os.path.join(reader.getDataRoot(),"braviz_data","tabular_data.sqlite")
     conn=sqlite3.connect(path)
     return conn
+
+def get_data_frame(columns,reader=None):
+    if type(columns) in (str,unicode):
+        columns=(columns,)
+    conn=get_connection(reader)
+    data=col=sql.read_sql("SELECT subject from SUBJECTS",conn,index_col="subject")
+    for var in columns:
+        query="""SELECT subject, value
+        FROM subjects NATURAL JOIN var_values
+        WHERE var_idx = (SELECT var_idx FROM variables WHERE var_name = ?)
+        """
+        col=sql.read_sql(query,conn,index_col="subject",params=(var,),coerce_float=True)
+        data[var]=col.astype(pd.np.float64)
+
+    conn.close()
+    return data
