1b505ce24dfe64897e324842073e00677a9d25c9
Diego A <devnull@localhost>
2014-05-06 12:41:47 -0400

    vitamins2

---
+++ concept_tests/find_vitamins2.py
@@ -0,0 +1,32 @@
+from __future__ import division
+
+__author__ = 'Diego'
+
+import os
+import nibabel as nib
+import numpy as np
+from scipy import ndimage
+import braviz
+from skimage import morphology
+
+os.chdir(r"C:\Users\Diego\Documents\prueba_free_surf_400")
+
+skull = nib.load("skull.nii.gz")
+data = skull.get_data()
+#===============
+
+#data2 = data
+
+data2 = ndimage.gaussian_filter(data,3)
+data2 = ndimage
+#=================
+viewer = braviz.visualization.simpleVtkViewer()
+vtk_image = braviz.readAndFilter.numpy2vtk_img(data2)
+plane_widget = viewer.addImg(vtk_image)
+plane_widget.SetResliceInterpolateToNearestNeighbour()
+plane_widget.set_orientation(0)
+viewer.start()
+plane_widget.set_orientation(1)
+viewer.start()
+plane_widget.set_orientation(2)
+viewer.start()
