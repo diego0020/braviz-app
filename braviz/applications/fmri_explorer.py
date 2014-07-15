dafd6ff79b5efd5fbcb72e3d8cf5417ead658b63
Diego A <devnull@localhost>
2014-07-15 08:41:58 -0500

    some advances with fmri_explore controls

--- braviz/applications/fmri_explorer.py
+++ braviz/applications/fmri_explorer.py
@@ -13,15 +13,35 @@ import numpy as np
 
 __author__ = 'Diego'
 
+class ListValidator(QtGui.QValidator):
+    def __init__(self,valid_options):
+        super(ListValidator,self).__init__()
+        self.valid = frozenset(valid_options)
+
+    def validate(self, QString, p_int):
+        str_value = str(QString)
+        if str_value in self.valid:
+            return QtGui.QValidator.Acceptable,p_int
+        else:
+            if len(str_value) == 0:
+                return QtGui.QValidator.Intermediate,p_int
+            try:
+                i = int(str_value)
+            except Exception:
+                return QtGui.QValidator.Invalid,p_int
+            else:
+                return QtGui.QValidator.Intermediate,p_int
+
 class FmriExplorer(QtGui.QMainWindow):
     def __init__(self,scenario,server_broadcast_address,server_receive_address):
         super(FmriExplorer,self).__init__()
 
         self.__reader = braviz.readAndFilter.BravizAutoReader()
 
+        self.__valid_ids = frozenset(str(i) for i in self.__reader.get("ids"))
         self.__current_subject = None
         self.__current_paradigm = None
-        self.__current_contrast = None
+        self.__current_contrast = 1
 
         self.ui = None
         self.three_d_widget = None
@@ -56,6 +76,16 @@ class FmriExplorer(QtGui.QMainWindow):
         for p in paradigms:
             self.ui.paradigm_combo.addItem(p)
         self.ui.paradigm_combo.setCurrentIndex(0)
+        self.ui.contrast_combo.clear()
+        self.ui.contrast_combo.activated.connect(self.update_fmri_data_view)
+        self.ui.paradigm_combo.activated.connect(self.update_fmri_data_view)
+
+        self.ui.subj_completer = QtGui.QCompleter(list(self.__valid_ids))
+        self.ui.subject_edit.setCompleter(self.ui.subj_completer)
+        self.ui.subj_validator = ListValidator(self.__valid_ids)
+        self.ui.subject_edit.setValidator(self.ui.subj_validator)
+        self.ui.subject_edit.editingFinished.connect(self.update_fmri_data_view)
+
 
     def start(self):
         self.three_d_widget.initialize_widget()
@@ -63,21 +93,49 @@ class FmriExplorer(QtGui.QMainWindow):
 
     def load_initial_view(self):
         self.__current_subject = self.__reader.get("ids")[0]
-        self.__current_paradigm = str(self.ui.paradigm_combo.currentText())
-        self.__current_contrast = 1
+        self.ui.subject_edit.setText(self.__current_subject)
+#        self.__current_paradigm = str(self.ui.paradigm_combo.currentText())
+#        self.__current_contrast = 1
         self.update_fmri_data_view()
 
     def update_fmri_data_view(self):
-        self.image_view.set_all(self.__current_subject,self.__current_paradigm,self.__current_contrast)
-        bold_image = self.__reader.get("BOLD",self.__current_subject,name=self.__current_paradigm)
-        self.time_plot.set_bold(bold_image)
-        spm_data = self.__reader.get("fmri",self.__current_subject,name=self.__current_paradigm,spm=True)
-        contrasts = spm_data.get_contrast_names()
+        log = logging.getLogger(__file__)
+        subj = str(self.ui.subject_edit.text())
+        if subj in self.__valid_ids:
+            self.__current_subject = subj
+        self.__current_paradigm = str(self.ui.paradigm_combo.currentText())
+        self.__current_contrast = self.ui.contrast_combo.currentIndex()+1
+
+        try:
+            spm_data = self.__reader.get("fmri",self.__current_subject,name=self.__current_paradigm,spm=True)
+            contrasts = spm_data.get_contrast_names()
+        except Exception:
+            log.warning("Couldn't read spm file")
+            spm_data = None
+        else:
+            self.ui.contrast_combo.clear()
+            for i in xrange(len(contrasts)):
+                self.ui.contrast_combo.addItem(contrasts[i+1])
+            if 1<=self.__current_contrast <= len(contrasts):
+                self.ui.contrast_combo.setCurrentIndex(self.__current_contrast-1)
+            else:
+                self.ui.contrast_combo.setCurrentIndex(0)
+                self.__current_contrast = 1  # 0+1
+        try:
+            self.image_view.set_all(self.__current_subject,self.__current_paradigm,self.__current_contrast)
+            bold_image = self.__reader.get("BOLD",self.__current_subject,name=self.__current_paradigm)
+        except Exception:
+            message = "%s not available for subject %s"%(self.__current_paradigm,self.__current_subject)
+            log.warning(message)
+            self.statusBar().showMessage(message,500)
+            bold_image = None
         #todo update contrasts combo
-        self.time_plot.set_spm(spm_data)
+        self.time_plot.clear()
+        self.time_plot.set_spm_and_bold(spm_data,bold_image)
 
     def handle_cursor_move(self,coords):
         self.statusBar().showMessage(str(coords))
+        self.time_plot.draw_bold_signal(coords)
 
 def run():
     import sys
--- braviz/visualization/fmri_timeseries.py
+++ braviz/visualization/fmri_timeseries.py
@@ -11,6 +11,7 @@ from matplotlib.figure import Figure
 import matplotlib.axes
 import matplotlib.gridspec as gridspec
 
+import braviz
 from itertools import izip
 
 import numpy as np
@@ -44,6 +45,7 @@ class TimeseriesPlot(FigureCanvas):
         #internal_data
         self.spm = None
         self.bold = None
+        self.volumes_times = None
 
         self.colors_dict = None
         self.__title = None
@@ -51,18 +53,48 @@ class TimeseriesPlot(FigureCanvas):
         self.axes.tick_params('y', left='off', right='off', labelleft='off', labelright='off')
         self.axes.tick_params('x', top='off', bottom='on', labelbottom='on', labeltop='off')
         self.axes.set_xlabel("Time (s.)")
-        self.axes.set_ylim(-1.5,1.5)
+        self.axes.set_ylim(-2,2,auto=False)
 
     def clear(self):
         self.axes.clear()
+        self.draw()
 
-    def set_bold(self,bold_image):
-        self.bold = bold_image
+    def _set_bold(self,bold_image):
+        if bold_image is None:
+            self.volumes_times = None
+            self.bold = None
+            return
+        self.bold = bold_image.get_data()
+        self.volumes_times = np.arange(0,self.bold.shape[3])
+        self.volumes_times *= self.spm.tr
+        self.volumes_times += self.spm.tr/2 # middle of acquisition
+        #HACK
+        if braviz.readAndFilter.PROJECT == "kmc40":
+            #ignore first volume
+            self.bold = self.bold[:,:,:,1:]
+            self.volumes_times=self.volumes_times[1:]
+        self.axes.set_xlim(0,self.spm.tr+self.volumes_times[-1],auto=False)
 
-    def set_spm(self,spm_struct):
+    def _set_spm(self,spm_struct):
         self.spm = spm_struct
 
+    def set_spm_and_bold(self,spm_file,bold_image):
+        self._set_spm(spm_file)
+        self._set_bold(bold_image)
+
     def draw_bold_signal(self,coordinates):
+        if self.bold is None:
+            return
+        #draw background
+        self.axes.clear()
+        self.axes.set_ylim(-2,2,auto=False)
+        self.axes.set_xlim(0,self.spm.tr+self.volumes_times[-1],auto=False)
+
+        coordinates = np.array(coordinates).astype(np.int)
+        signal = self.bold[coordinates[0],coordinates[1],coordinates[2],:]
+        normalized_signal = (signal - np.mean(signal))/np.std(signal)
+        self.axes.plot(self.volumes_times,normalized_signal)
+        self.draw()
         pass
 
     def add_perm_bold_signal(self,coordinates):
