7504fe7102b81d32f9c236dc39413dd84ac9c7da
Diego A <devnull@localhost>
2014-09-10 11:57:26 -0500

    parallel coordinates prototype

--- concept_tests/d3_parallel_coordinates.py
+++ concept_tests/d3_parallel_coordinates.py
@@ -1,17 +1,49 @@
 
 import tornado.ioloop
 import tornado.web
+import os
+import braviz.readAndFilter.tabular_data as tab_data
+import json
 
 __author__ = 'da.angulo39'
 
+vars = [1982,1002,1003,1004]
+
+data = tab_data.get_data_frame_by_index(vars)
+data.dropna(inplace=True)
+cols = data.columns
+cols2=list(cols)
+cols2[0]="species"
+data.columns=cols2
+col0 = cols[0]
+labels = tab_data.get_labels_dict(vars[0])
+
+col0 = cols2[0]
+data[col0]=data[col0].map(labels)
+
+json_data = data.to_json(orient="records")
+
+caths = data[col0].unique()
+caths_json = json.dumps(list(caths))
+
+attrs=list(data.columns[1:])
+attrs_json = json.dumps(attrs)
+
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.render("parallel_coordinates.html",data=json_data,caths=caths_json,vars=attrs_json,cath_name=col0)
 
-application = tornado.web.Application([
-    (r"/", MainHandler),
-])
+
+
+settings = {
+    "static_path":os.path.join(os.path.dirname(__file__),"web_static")
+}
 
 if __name__ == "__main__":
+    application = tornado.web.Application([
+    (r"/", MainHandler),
+    ],
+    **settings)
     application.listen(8100)
     tornado.ioloop.IOLoop.instance().start()
