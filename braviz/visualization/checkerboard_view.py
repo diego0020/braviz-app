cc52ed1cf9b10d5f7293c0c1a6897aa076b2af3a
Diego A <devnull@localhost>
2014-09-18 21:55:24 -0500

    check registration working skeleton addresses #8

--- braviz/visualization/checkerboard_view.py
+++ braviz/visualization/checkerboard_view.py
@@ -45,8 +45,8 @@ class CheckbordView(object):
         self.__lut = self.reader.get("APARC",None,lut=True)
         self.__color_labels1 = vtk.vtkImageMapToColors()
         self.__color_labels2 = vtk.vtkImageMapToColors()
-        self.__color1_nominal.SetLookupTable(self.__lut)
-        self.__color2_nominal.SetLookupTable(self.__lut)
+        self.__color_labels1.SetLookupTable(self.__lut)
+        self.__color_labels2.SetLookupTable(self.__lut)
 
         self.__color1_nominal = False
         self.__color2_nominal = False
@@ -60,31 +60,130 @@ class CheckbordView(object):
         self.__img1_params=None
         self.__img2_params=None
 
+        self.__outline = vtk.vtkOutlineFilter()
+        self.__outline_mapper = vtk.vtkPolyDataMapper()
+        self.__outline_mapper.SetInputConnection(self.__outline.GetOutputPort())
+        self.__outline_actor= vtk.vtkActor()
+        self.__outline_actor.SetMapper(self.__outline_mapper)
+        self.ren.AddActor(self.__outline_actor)
+        self.reset_camera(0)
+
+    __camera_positions_dict = {
+        0: ((-3.5, 0, 13), (157, 154, 130), (0, 0, 1)),
+        2: ((-3.5, 0, 10), (250, 0, 10), (0, 0, 1)),
+        1: ((-3.5, 0, 10), (-250, 0, 10), (0, 0, 1)),
+        4: ((-3.5, 0, 10), (-3.5, -200, 10), (0, 0, 1)),
+        3: ((-3.5, 0, 10), (-3.5, 200, 10), (0, 0, 1)),
+        5: ((-3, 0, 3), (-3, 0, 252), (0, 1, 0)),
+        6: ((-3, 0, 3), (-3, 0, -252), (0, 1, 0)),
+    }
+
+    def reset_camera(self, position):
+        """resets the current camera to standard locations. Position may be:
+        0: initial 3d view
+        1: left
+        2: right
+        3: front
+        4: back
+        5: top
+        6: bottom"""
+
+        focal, position, viewup = self.__camera_positions_dict[position]
+
+        cam1 = self.ren.GetActiveCamera()
+        cam1.SetFocalPoint(focal)
+        cam1.SetPosition(position)
+        cam1.SetViewUp(viewup)
+
+        self.ren.ResetCameraClippingRange()
+        self.ren_win.Render()
+
     def update_pipeline(self):
-        if self.__img1 is not None:
-            if self.__color1_nominal:
-                self.__color_labels1.SetInputData(self.__img1)
-
-
-
         if self.__plane_widget is None:
             self.create_image_plane_widget()
 
+        if self.__img1 is None and self.__img2 is None:
+            self.__plane_widget.Off()
+            return
+        else:
+            self.__plane_widget.On()
+
+        if self.__img1 is not None:
+            self.__outline.SetInputData(self.__img1)
+            if self.__color1_nominal:
+                self.__color_labels1.SetInputData(self.__img1)
+                if self.__img2 is None:
+                    self.__plane_widget.SetInputConnection(self.__color_labels1.GetOutputPort())
+                else:
+                    self.__color_labels1.Update()
+                    self.__checkboard_view.SetInput1Data(self.__color_labels1.GetOutput())
+            else:
+                self.__color_wl1.SetInputData(self.__img1)
+                w,l = get_window_level(self.__img1)
+                self.__color_wl1.SetWindow(w)
+                self.__color_wl1.SetLevel(l)
+                if self.__img2 is None:
+                    self.__color_wl1.Update()
+                    self.__plane_widget.SetInputData(self.__color_wl1.GetOutput())
+                    #self.__plane_widget.SetInputConnection(self.__color_wl1.GetOutputPort())
+                else:
+                    self.__color_wl1.Update()
+                    self.__checkboard_view.SetInput1Data(self.__color_wl1.GetOutput())
+
+        if self.__img2 is not None:
+            if self.__img1 is not None:
+                self.__reslice2.SetOutputOrigin(self.__img1.GetOrigin())
+                self.__reslice2.SetOutputSpacing(self.__img1.GetSpacing())
+                self.__reslice2.SetOutputExtent(self.__img1.GetExtent())
+                if self.__color2_nominal:
+                    self.__reslice2.SetInterpolationModeToNearestNeighbor()
+                else:
+                    self.__reslice2.SetInterpolationModeToCubic()
+                self.__reslice2.SetInputData(self.__img2)
+            else:
+                self.__outline.SetInputData(self.__img2)
 
 
+            #----------------------------------
+            if self.__color2_nominal:
+                if self.__img1 is None:
+                    self.__color_labels2.SetInputData(self.__img2)
+                    self.__plane_widget.SetInputConnection(self.__color_labels2.GetOutputPort())
+                else:
+                    self.__color_labels2.SetInputConnection(self.__reslice2.GetOutputPort())
+                    self.__color_labels2.Update()
+                    self.__checkboard_view.SetInput2Data(self.__color_labels2.GetOutput())
+            else:
+                w,l = get_window_level(self.__img2)
+                self.__color_wl2.SetWindow(w)
+                self.__color_wl2.SetLevel(l)
+                if self.__img1 is None:
+                    self.__color_wl2.SetInputData(self.__img2)
+                    self.__plane_widget.SetInputConnection(self.__color_wl2.GetOutputPort())
+                else:
+                    self.__color_wl2.SetInputConnection(self.__reslice2.GetOutputPort())
+                    self.__color_wl2.Update()
+                    self.__checkboard_view.SetInput2Data(self.__color_wl2.GetOutput())
+        if self.__img1 is not None and self.__img2 is not None:
+            self.__checkboard_view.Update()
+            #print self.__checkboard_view.GetOutput()
+            self.__plane_widget.SetInputData(self.__checkboard_view.GetOutput())
 
 
     def create_image_plane_widget(self):
         if self.__plane_widget is None:
             self.__plane_widget = braviz.visualization.persistentImagePlane(self.__orientation)
             self.__plane_widget.SetInteractor(self.iren)
+            self.__plane_widget.GetColorMap().SetLookupTable(None)
             self.__plane_widget.On()
+
 
     @do_and_render
     def set_img1(self,subj,mod,contrast=None):
         params = (subj,mod.upper(),contrast)
         if params == self.__img1_params:
             return
+        self.__img1_params = params
         if contrast is not None:
             raise NotImplementedError
         self.__img1 = self.reader.get(mod,subj,format="vtk",space=self.__current_space)
@@ -94,13 +193,14 @@ class CheckbordView(object):
             self.__color1_nominal = False
 
         self.update_pipeline()
-        self.__img1_params = params
+
 
     @do_and_render
     def set_img2(self,subj,mod,contrast=None):
         params = (subj,mod.upper(),contrast)
         if params == self.__img2_params:
             return
+        self.__img2_params = params
         if contrast is not None:
             raise NotImplementedError
         self.__img2 = self.reader.get(mod,subj,format="vtk",space=self.__current_space)
@@ -110,7 +210,7 @@ class CheckbordView(object):
             self.__color2_nominal = False
 
         self.update_pipeline()
-        self.__img2_params = params
+
 
     def set_orientation(self,orientation_int):
         pass
@@ -121,7 +221,11 @@ class CheckbordView(object):
 
 
 def get_window_level(img):
-    return 0,500
+    stats = vtk.vtkImageHistogramStatistics()
+    stats.SetInputData(img)
+    stats.Update()
+    _,w = stats.GetAutoRange()
+    return w,w/2
 
 
 class QCheckViewer(QFrame):
