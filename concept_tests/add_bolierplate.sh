f91802976f7c73b67e9a2292ddca5b43bcd65293
diego <devnull@localhost>
2015-01-14 22:28:11 +0000

    license boilerplate script before running

---
+++ concept_tests/add_bolierplate.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+test_if_has_bp () {
+    local readonly boilerplate=license_boilerplate.txt
+    local bp_text=$(cat $boilerplate)
+    local file=$1
+    local file_pre=$(cat $1 | head -n 18 )
+    if [ "$bp_text" = "$file_pre" ] ; then
+    echo "1"
+    else
+    echo "0"
+    fi
+}
+
+add_bp_to_file () {
+    local test=$( test_if_has_bp $1)
+    if [ "$test" -eq "0" ]
+    then
+        local boilerplate=license_boilerplate.txt
+        local file=$1
+        echo adding bolirplate to $file
+        cat $boilerplate $file > {$file}.bp
+        rm $file
+        mv {$file}.bp $file
+    else
+        echo $1 already has boilerplate
+    fi
+}
+
+add_to_braviz () {
+    braviz_files=$(find ../braviz -name "*.py")
+    for f in ${braviz_files[*]}
+    do
+        echo add to $f
+        add_bp_to_file $f
+        echo
+    done
+
+
+}
+
+main () {
+    readonly DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+    cd $DIR
+    add_to_braviz
+
+}
+
+main
