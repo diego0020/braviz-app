8f3a4c1c617eb8b4450644ed295579a811ee20ca
diego <devnull@localhost>
2015-01-15 13:06:19 +0000

    added LGPL boilerplate

--- braviz/interaction/tk_tooltip.py
+++ braviz/interaction/tk_tooltip.py
@@ -1,3 +1,21 @@
+##############################################################################
+#    Braviz, Brain Data interactive visualization                            #
+#    Copyright (C) 2014  Diego Angulo                                        #
+#                                                                            #
+#    This program is free software: you can redistribute it and/or modify    #
+#    it under the terms of the GNU Lesser General Public License as          #
+#    published by  the Free Software Foundation, either version 3 of the     #
+#    License, or (at your option) any later version.                         #
+#                                                                            #
+#    This program is distributed in the hope that it will be useful,         #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of          #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           #
+#    GNU Lesser General Public License for more details.                     #
+#                                                                            #
+#    You should have received a copy of the GNU Lesser General Public License#
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.   #
+##############################################################################
+
 
 # Originally copied from http://code.activestate.com/recipes/576688-tooltip-for-tkinter/
 # Licensed under the MIT License http://opensource.org/licenses/MIT
