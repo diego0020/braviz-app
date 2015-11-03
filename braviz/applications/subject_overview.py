09bf638c8468b0117d0722e87e9eececf7ae1a67
diego <da.angulo39@uniandes.edu.co>
2015-11-02 20:54:54 -0500

    wrong import

--- braviz/applications/subject_overview.py
+++ braviz/applications/subject_overview.py
@@ -34,7 +34,7 @@ import os
 from braviz.visualization.simple_vtk import save_ren_win_picture
 import braviz.readAndFilter.tabular_data as braviz_tab_data
 import braviz.readAndFilter.user_data as braviz_user_data
-from readAndFilter.bundles_db import remove_bundle, get_bundle_name
+from braviz.readAndFilter.bundles_db import remove_bundle, get_bundle_name
 from braviz.interaction.qt_guis.subject_overview import Ui_subject_overview
 from braviz.interaction.qt_models import SubjectsTable, SubjectDetails, StructureTreeModel, \
     SimpleCheckModel,BundlesSelectionList
